6aaea39cef1b041ec922598a59696d3adfaeb873
Bogdan Condurache <bogdan@robocorp.com>
2024-06-05 10:08:44 +0200

    remove parsepypi

--- agents/dependency-due-diligence/license-guru/actions.py
+++ agents/dependency-due-diligence/license-guru/actions.py
@@ -19,26 +19,6 @@ def get_metadata(package_name: str) -> str:
     return json.dumps(resp.json())
 
 
-# @action
-# def parse_pypi(package_name: str) -> str:
-#     """
-#     Retrieves the HTML page on PyPi for the given package.
-
-#     Args:
-#         package_name (str): Name of the package to scan
-
-#     Returns:
-#         Returns the HTML page of the package on PyPi that needs to be parsed.
-#     """
-#     resp = requests.get(f"https://pypi.org/project/{package_name}/")
-#     resp.raise_for_status()
-#     soup = BeautifulSoup(resp.text)
-#     content = soup.find_all("div", class_="tabs-container")
-#     if len(content) == 0:
-#         return "No content was found"
-#     return resp.text
-
-
 @action
 def parse_snyk(package_name: str) -> str:
     """
