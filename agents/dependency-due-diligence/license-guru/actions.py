a87c0a82f88f00b64fd4e7cffa39e544e21e12b7
Bogdan Condurache <bogdan@robocorp.com>
2024-06-05 08:59:47 +0200

    rename variable

--- agents/dependency-due-diligence/license-guru/actions.py
+++ agents/dependency-due-diligence/license-guru/actions.py
@@ -1,51 +1,52 @@
 from sema4ai.actions import action
 import requests
+import json
 
 @action
-def get_metadata(pkg_name: str) -> str:
+def get_metadata(package_name: str) -> str:
     """
     Retrieves the PyPi metadata for the given package.
 
     Args:
-        pkg_name (str): Name of the package to scan
+        package_name (str): Name of the package to scan
 
     Returns:
         Returns the JSON metadata of the package as a string.
     """
-    resp = requests.get(f"https://pypi.org/pypi/{pkg_name}/json")
+    resp = requests.get(f"https://pypi.org/pypi/{package_name}/json")
     resp.raise_for_status()
-    return resp.json()
+    return json.dumps(resp.json())
 
 
 @action
-def parse_pypi(pkg_name: str) -> str:
+def parse_pypi(package_name: str) -> str:
     """
     Retrieves the HTML page on PyPi for the given package.
 
     Args:
-        pkg_name (str): Name of the package to scan
+        package_name (str): Name of the package to scan
 
     Returns:
         Returns the HTML page of the package on PyPi that needs to be parsed.
     """
-    resp = requests.get(f"https://pypi.org/project/{pkg_name}/")
+    resp = requests.get(f"https://pypi.org/project/{package_name}/")
     resp.raise_for_status()
     return resp.text
 
 
 @action
-def parse_snyk(pkg_name: str) -> str:
+def parse_snyk(package_name: str) -> str:
     """
     Retrieves the HTML page on Snyk for the given package. It gives information
     regarding the security issues and CVEs for the package. It acts as a
     security scan
 
     Args:
-        pkg_name (str): Name of the package to scan
+        package_name (str): Name of the package to scan
 
     Returns:
         Returns the HTML page of the package on PyPi that needs to be parsed.
     """
-    resp = requests.get(f"https://pypi.org/project/{pkg_name}/")
+    resp = requests.get(f"https://pypi.org/project/{package_name}/")
     resp.raise_for_status()
     return resp.text
