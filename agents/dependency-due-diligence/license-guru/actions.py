691ba8839a311e2d4fc44b5cfb75c666e56e132f
Bogdan Condurache <bogdan@robocorp.com>
2024-06-05 09:56:47 +0200

    comment out parse_pypi

--- agents/dependency-due-diligence/license-guru/actions.py
+++ agents/dependency-due-diligence/license-guru/actions.py
@@ -19,32 +19,10 @@ def get_metadata(package_name: str) -> str:
     return json.dumps(resp.json())
 
 
-@action
-def parse_pypi(package_name: str) -> str:
-    """
-    Retrieves the HTML page on PyPi for the given package.
-
-    Args:
-        package_name (str): Name of the package to scan
-
-    Returns:
-        Returns the HTML page of the package on PyPi that needs to be parsed.
-    """
-    resp = requests.get(f"https://pypi.org/project/{package_name}/")
-    resp.raise_for_status()
-    soup = BeautifulSoup(resp.text)
-    content = soup.find_all("div", class_="tabs-container")
-    if len(content) == 0:
-        return "No content was found"
-    return resp.text
-
-
 # @action
-# def parse_snyk(package_name: str) -> str:
+# def parse_pypi(package_name: str) -> str:
 #     """
-#     Retrieves the HTML page on Snyk for the given package. It gives information
-#     regarding the security issues and CVEs for the package. It acts as a
-#     security scan
+#     Retrieves the HTML page on PyPi for the given package.
 
 #     Args:
 #         package_name (str): Name of the package to scan
@@ -52,10 +30,32 @@ def parse_pypi(package_name: str) -> str:
 #     Returns:
 #         Returns the HTML page of the package on PyPi that needs to be parsed.
 #     """
-#     resp = requests.get(f"https://snyk.io/advisor/python/{package_name}/")
+#     resp = requests.get(f"https://pypi.org/project/{package_name}/")
 #     resp.raise_for_status()
 #     soup = BeautifulSoup(resp.text)
-#     content = soup.find_all("div", class_="package-container")
+#     content = soup.find_all("div", class_="tabs-container")
 #     if len(content) == 0:
 #         return "No content was found"
-#     return content[0]
+#     return resp.text
+
+
+@action
+def parse_snyk(package_name: str) -> str:
+    """
+    Retrieves the HTML page on Snyk for the given package. It gives information
+    regarding the security issues and CVEs for the package. It acts as a
+    security scan
+
+    Args:
+        package_name (str): Name of the package to scan
+
+    Returns:
+        Returns the HTML page of the package on PyPi that needs to be parsed.
+    """
+    resp = requests.get(f"https://snyk.io/advisor/python/{package_name}/")
+    resp.raise_for_status()
+    soup = BeautifulSoup(resp.text)
+    content = soup.find_all("div", class_="package-container")
+    if len(content) == 0:
+        return "No content was found"
+    return content[0]
