7029ce42ba8cfd91e83d8a7a2c7b2ed64e257980
dhiganthrao <dhiganth00@gmail.com>
2024-07-15 11:38:29 -0700

    Formatted .py files with black

--- agents/marketing-agent/actions/website-scraper/actions.py
+++ agents/marketing-agent/actions/website-scraper/actions.py
@@ -13,141 +13,154 @@ import re
 from helpers import TechCrunchScraper, MediumScraper
 import json
 from glob import glob
+
+
 @action
-def scrape_data_tc(tag: str, num_articles:int =10) -> bool:
+def scrape_data_tc(tag: str, num_articles: int = 10) -> bool:
     """
     Scrape TechCrunch articles based on a specific tag.
-    
+
     Args:
         tag (str): The tag to search for.
         num_articles (int): The number of articles to scrape.
-    
+
     Returns:
         bool: True if the action was successful, False otherwise.
     """
     scraper = TechCrunchScraper(tag)
     article_links = scraper.fetch_article_links(num_articles)
-    dir_to_save = os.path.join(scraper.get_parent_dir(), scraper.get_tag().split('/')[-1])
+    dir_to_save = os.path.join(
+        scraper.get_parent_dir(), scraper.get_tag().split("/")[-1]
+    )
     os.makedirs(dir_to_save, exist_ok=True)
-    with open(os.path.join(dir_to_save, 'techcrunch_links.txt'), 'w+') as f:
+    with open(os.path.join(dir_to_save, "techcrunch_links.txt"), "w+") as f:
         for link in article_links:
-            f.write(link + '\n')
+            f.write(link + "\n")
     for link in article_links:
         content = scraper.scrape_article_content(link)
-        filename = os.path.join(dir_to_save, 'techcrunch-' + link.split('/')[-2] + '.txt')
+        filename = os.path.join(
+            dir_to_save, "techcrunch-" + link.split("/")[-2] + ".txt"
+        )
         if os.path.exists(filename):
             if os.path.getsize(filename) > 0:
                 print(f"Article already exists: {filename}")
                 continue
             else:
-                if content != '':
+                if content != "":
                     scraper.save_article_content(filename, content)
                     print(f"Article saved: {filename}")
-                    scraper.save_title(filename, os.path.join(dir_to_save, 'titles.json'))
+                    scraper.save_title(
+                        filename, os.path.join(dir_to_save, "titles.json")
+                    )
                     time.sleep(2)
                 else:
                     print(f"Article not saved: {filename}, could not scrape content.")
-                    continue   
+                    continue
         else:
             scraper.save_article_content(filename, content)
             print(f"Article saved: {filename}")
-            scraper.save_title(filename, os.path.join(dir_to_save, 'titles.json'))
+            scraper.save_title(filename, os.path.join(dir_to_save, "titles.json"))
             time.sleep(2)
     return True
 
+
 @action
-def scrape_data_medium(tag: str, num_articles: int=10) -> bool:
+def scrape_data_medium(tag: str, num_articles: int = 10) -> bool:
     """
     Scrape articles from a Medium tag URL and save them to disk.
-    
+
     Args:
         tag (str): Tag to scrape articles from
         num_articles (int): Number of articles to scrape
-    
+
     Returns:
         bool: True if the action was successful, False otherwise
     """
     scraper = MediumScraper(tag)
     article_links = scraper.fetch_article_links(num_articles)
-    dir_to_save = os.path.join(scraper.get_parent_dir(), scraper.get_tag().split('/')[-1])
+    dir_to_save = os.path.join(
+        scraper.get_parent_dir(), scraper.get_tag().split("/")[-1]
+    )
     os.makedirs(dir_to_save, exist_ok=True)
-    with open(os.path.join(dir_to_save, 'medium_links.txt'), 'w+') as f:
+    with open(os.path.join(dir_to_save, "medium_links.txt"), "w+") as f:
         for link in article_links:
-            f.write(link + '\n')
+            f.write(link + "\n")
     for link in article_links:
         content = scraper.scrape_article_content(link)
         # Create a simple filename from the article link
         # remove the number from the filename
-        filename = os.path.join(dir_to_save, 'medium-' + link.split('/')[-1] + '.txt')
+        filename = os.path.join(dir_to_save, "medium-" + link.split("/")[-1] + ".txt")
         if os.path.exists(filename):
             # member only story, can try to save it again
             if os.path.getsize(filename) > 0:
                 print(f"Article already exists: {filename}")
                 continue
             else:
-                if content != '':
+                if content != "":
                     scraper.save_article_content(filename, content)
                     print(f"Article saved: {filename}")
-                    scraper.save_title(filename, os.path.join(dir_to_save, 'titles.json'))
+                    scraper.save_title(
+                        filename, os.path.join(dir_to_save, "titles.json")
+                    )
                     time.sleep(2)
                 else:
                     print(f"Article not saved: {filename}, could not scrape content.")
-                    continue   
+                    continue
             # print(f"Article already exists: {filename}")
             # continue
         else:
             scraper.save_article_content(filename, content)
             print(f"Article saved: {filename}")
-            scraper.save_title(filename, os.path.join(dir_to_save, 'titles.json'))
+            scraper.save_title(filename, os.path.join(dir_to_save, "titles.json"))
             time.sleep(2)
     return True
+
 
 @action
 def load_json(tag: str, filename: str) -> str:
     """
     Load a JSON file.
-    
+
     Args:
         tag (str): The tag to search for.
         filename (str): The filename of the JSON file to load.
-    
+
     Returns:
         String represeeing the loaded JSON data.
     """
     parent_dir = "/tmp/tags"
-    tag = tag.lower().replace(' ', '-')
+    tag = tag.lower().replace(" ", "-")
     try:
-        with open(os.path.join(parent_dir, tag, filename), 'r') as f:
+        with open(os.path.join(parent_dir, tag, filename), "r") as f:
             data = json.load(f)
         return str(data)
     except FileNotFoundError:
         raise FileNotFoundError(f"File not found: {filename}")
     except Exception as e:
         raise Exception(f"Error loading JSON file: {e}")
-    
+
+
 @action
 def load_articles(tag: str) -> str:
     """
     Load all articles from a specific tag.
-    
+
     Args:
         tag (str): The tag to search for.
-    
+
     Returns:
         String representing the loaded articles.
     """
     parent_dir = "/tmp/tags"
-    tag = tag.lower().replace(' ', '-')
-    content = ''
+    tag = tag.lower().replace(" ", "-")
+    content = ""
     try:
-        for filename in glob(os.path.join(parent_dir, tag, '*.txt')):
-            with open(filename, 'r') as f:
+        for filename in glob(os.path.join(parent_dir, tag, "*.txt")):
+            with open(filename, "r") as f:
                 data = f.read()
-                content += data + '\n-----------------\n'
+                content += data + "\n-----------------\n"
         return content
     except FileNotFoundError:
         raise FileNotFoundError(f"No articles found for tag: {tag}")
     except Exception as e:
         raise Exception(f"Error loading articles: {e}")
-            
-        
--- agents/marketing-agent/actions/website-scraper/helpers.py
+++ agents/marketing-agent/actions/website-scraper/helpers.py
@@ -5,150 +5,162 @@ import re
 import json
 import os
 
+
 class BaseScraper:
-      def __init__(self, url):
-         self.response = requests.get(url)
-         self.soup = BeautifulSoup(self.response.text, 'html.parser')
-         self.parent_dir = "/tmp/tags"
-   
-      # children must override these methods
-      def fetch_article_links(self, num_articles=10):
-         raise NotImplementedError
-   
-      def scrape_article_content(self, article_url):
-         raise NotImplementedError
-      
-      def save_article_content(self, filename, content):
-         with open(filename, 'w+', encoding='utf-8') as file:
+    def __init__(self, url):
+        self.response = requests.get(url)
+        self.soup = BeautifulSoup(self.response.text, "html.parser")
+        self.parent_dir = "/tmp/tags"
+
+    # children must override these methods
+    def fetch_article_links(self, num_articles=10):
+        raise NotImplementedError
+
+    def scrape_article_content(self, article_url):
+        raise NotImplementedError
+
+    def save_article_content(self, filename, content):
+        with open(filename, "w+", encoding="utf-8") as file:
             file.write(content)
-      
-      def save_title(self, input_filename, path_to_save):
-         filename = input_filename.split('/')[-1].split('.')[0].split('-')
-         site = filename[0]
-         title = filename[1:-1] if site == 'medium' else filename[1:]
-         title = ' '.join(title)
-         title = title.title()
-         if not os.path.exists(path_to_save):
-            with open(path_to_save, 'w+') as f:
-               json.dump({}, f)
-         with open(path_to_save, 'r') as f:
+
+    def save_title(self, input_filename, path_to_save):
+        filename = input_filename.split("/")[-1].split(".")[0].split("-")
+        site = filename[0]
+        title = filename[1:-1] if site == "medium" else filename[1:]
+        title = " ".join(title)
+        title = title.title()
+        if not os.path.exists(path_to_save):
+            with open(path_to_save, "w+") as f:
+                json.dump({}, f)
+        with open(path_to_save, "r") as f:
             data = json.load(f)
             if site not in data:
-               data[site] = []
+                data[site] = []
             data[site].append(title)
-         with open(path_to_save, 'w+') as f:
+        with open(path_to_save, "w+") as f:
             json.dump(data, f, indent=4)
-         print(f"Title: {title} saved.")
+        print(f"Title: {title} saved.")
 
-      def get_parent_dir(self):
-          return self.parent_dir
+    def get_parent_dir(self):
+        return self.parent_dir
+
 
 class TechCrunchScraper(BaseScraper):
-   def __init__(self, tag):
-      self.tag = tag.lower().replace(' ', '-')
-      url = f'https://techcrunch.com/tag/{self.tag}/'
-      super().__init__(url)
-   
-   def fetch_article_links(self, num_articles=10):
-      figures = self.soup.find_all('figure', class_="wp-block-post-featured-image")
+    def __init__(self, tag):
+        self.tag = tag.lower().replace(" ", "-")
+        url = f"https://techcrunch.com/tag/{self.tag}/"
+        super().__init__(url)
 
-      # Define a regex pattern to match URLs that contain a year after the domain
-      pattern = re.compile(r'https://techcrunch\.com/\d{4}/')
+    def fetch_article_links(self, num_articles=10):
+        figures = self.soup.find_all("figure", class_="wp-block-post-featured-image")
 
-      # Initialize a list to store the links
-      links = set()
+        # Define a regex pattern to match URLs that contain a year after the domain
+        pattern = re.compile(r"https://techcrunch\.com/\d{4}/")
 
-      # Iterate over each figure found
-      for figure in figures:
-         if num_articles is not None and len(links) >= num_articles:
-               break
-         a_tag = figure.find('a', href=True)
-         if a_tag and pattern.search(a_tag['href']):
-               links.add(a_tag['href'])
+        # Initialize a list to store the links
+        links = set()
 
-      return links
-   
-   def scrape_article_content(self, article_url):
-      response = requests.get(article_url)
-      article_soup = BeautifulSoup(response.text, 'html.parser')
+        # Iterate over each figure found
+        for figure in figures:
+            if num_articles is not None and len(links) >= num_articles:
+                break
+            a_tag = figure.find("a", href=True)
+            if a_tag and pattern.search(a_tag["href"]):
+                links.add(a_tag["href"])
 
-      # Find the main content div
-      content_div = article_soup.find('div', class_='entry-content wp-block-post-content is-layout-flow wp-block-post-content-is-layout-flow')
+        return links
 
-      # Initialize an empty list to hold all text pieces
-      article_content = []
+    def scrape_article_content(self, article_url):
+        response = requests.get(article_url)
+        article_soup = BeautifulSoup(response.text, "html.parser")
 
-      if content_div:
-         # Extract text from all <p> tags and header tags
-         text_elements = content_div.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
-         for element in text_elements:
-            text = element.get_text(strip=True)
-            if element.name in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']:
-                  # Add formatting for headers
-                  article_content.append(f"**{text}**\n")
-            else:
-                  # Add text with a newline for paragraphs
-                  article_content.append(f"{text}\n")
+        # Find the main content div
+        content_div = article_soup.find(
+            "div",
+            class_="entry-content wp-block-post-content is-layout-flow wp-block-post-content-is-layout-flow",
+        )
 
-      # Join all pieces of text into a single string
-      full_article = '\n'.join(article_content)
-      return full_article
-   
-   def get_tag(self):
-      return self.tag
-   
+        # Initialize an empty list to hold all text pieces
+        article_content = []
+
+        if content_div:
+            # Extract text from all <p> tags and header tags
+            text_elements = content_div.find_all(
+                ["p", "h1", "h2", "h3", "h4", "h5", "h6"]
+            )
+            for element in text_elements:
+                text = element.get_text(strip=True)
+                if element.name in ["h1", "h2", "h3", "h4", "h5", "h6"]:
+                    # Add formatting for headers
+                    article_content.append(f"**{text}**\n")
+                else:
+                    # Add text with a newline for paragraphs
+                    article_content.append(f"{text}\n")
+
+        # Join all pieces of text into a single string
+        full_article = "\n".join(article_content)
+        return full_article
+
+    def get_tag(self):
+        return self.tag
+
 
 class MediumScraper(BaseScraper):
-   def __init__(self, tag):
-      self.tag = tag.lower().replace(' ', '-')
-      url = f'https://medium.com/tag/{self.tag}'
-      super().__init__(url)
+    def __init__(self, tag):
+        self.tag = tag.lower().replace(" ", "-")
+        url = f"https://medium.com/tag/{self.tag}"
+        super().__init__(url)
 
-   def fetch_article_links(self, num_articles=10):
-      # Find all <div> tags with the role="link"
-      divs = self.soup.find_all('div', attrs={'role': 'link'})
-      # print(divs)
-      links = []
+    def fetch_article_links(self, num_articles=10):
+        # Find all <div> tags with the role="link"
+        divs = self.soup.find_all("div", attrs={"role": "link"})
+        # print(divs)
+        links = []
 
-      # Iterate over each div found
-      for div in divs:
-         if num_articles is not None and len(links) >= num_articles:
-            break
-         # Check if 'link-href' attribute exists
-         data_href = div.get('data-href')
-         if data_href:
-            # Append the full URL (considering relative URLs)
-            full_url = f"https://medium.com{data_href}" if data_href.startswith('/') else data_href
-            links.append(full_url)
+        # Iterate over each div found
+        for div in divs:
+            if num_articles is not None and len(links) >= num_articles:
+                break
+            # Check if 'link-href' attribute exists
+            data_href = div.get("data-href")
+            if data_href:
+                # Append the full URL (considering relative URLs)
+                full_url = (
+                    f"https://medium.com{data_href}"
+                    if data_href.startswith("/")
+                    else data_href
+                )
+                links.append(full_url)
 
-      return links
-   
-   def scrape_article_content(self, article_url):
-      response = requests.get(article_url)
-      article_soup = BeautifulSoup(response.text, 'html.parser')
+        return links
 
-      # Find the main article tag
-      article_tag = article_soup.find('article')
+    def scrape_article_content(self, article_url):
+        response = requests.get(article_url)
+        article_soup = BeautifulSoup(response.text, "html.parser")
 
-      # Initialize an empty list to hold all text pieces
-      article_content = []
+        # Find the main article tag
+        article_tag = article_soup.find("article")
 
-      if article_tag:
-         # Extract text from all <p> tags and header tags within the article tag
-         text_elements = article_tag.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
-         for element in text_elements:
-            text = element.get_text(strip=True)
-            if element.name in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']:
-                  # Add formatting for headers
-                  article_content.append(f"**{text}**\n")
-            else:
-                  # Add text with a newline for paragraphs
-                  article_content.append(f"{text}\n")
+        # Initialize an empty list to hold all text pieces
+        article_content = []
 
-      # Join all pieces of text into a single string
-      full_article = ''.join(article_content)
-      return full_article
-   
-   def get_tag(self):
-      return self.tag
+        if article_tag:
+            # Extract text from all <p> tags and header tags within the article tag
+            text_elements = article_tag.find_all(
+                ["p", "h1", "h2", "h3", "h4", "h5", "h6"]
+            )
+            for element in text_elements:
+                text = element.get_text(strip=True)
+                if element.name in ["h1", "h2", "h3", "h4", "h5", "h6"]:
+                    # Add formatting for headers
+                    article_content.append(f"**{text}**\n")
+                else:
+                    # Add text with a newline for paragraphs
+                    article_content.append(f"{text}\n")
 
+        # Join all pieces of text into a single string
+        full_article = "".join(article_content)
+        return full_article
+
+    def get_tag(self):
+        return self.tag
